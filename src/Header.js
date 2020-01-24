c1b9bb7ba1d16c89e573d1810393c8a5244841b0
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2020-01-24 16:18:30 +0300

    fix styles

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div style={{ 'background-color': 'blue' , 'border': 'double'}}>
+        <div style={{ 'background-color': 'blue' , 'border': 'double'}} className="header">
             <p>Our slide show</p>
         </div>
     )
